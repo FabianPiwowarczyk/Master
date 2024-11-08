3e09c4acdbde96e18b241bbd39edd281d1a6e473
Fabian <fg.piwowarczyk@gmail.com>
2024-11-08 15:04:51 +0100

    start avk

--- gridding/functions.py
+++ gridding/functions.py
@@ -1,2 +1,31 @@
 from .plot_lv3 import plot_lv3_data, plot_dif, combined_plot
 from .grid_func import monthly_mean
+
+
+def replot_all():
+    monthly_mean(5, 5, 0,
+                 'gosat_data/IUP-GHG-L2-N2O-GOSAT2-FOCAL-2020',
+                 'xn2o', 'longitude', 'latitude', 'gosat')
+
+    monthly_mean(5, 5, 0,
+                 'finished_data/2020_',
+                 'total_column', 'lon', 'lat', 'iasi')
+
+    plot_lv3_data(5, 5, 0,
+                  'monthly_means/gosat_{}_{}x{}_th{}.nc', 'gosat', 280, 340)
+
+    plot_lv3_data(5, 5, 0,
+                  'monthly_means/iasi_{}_{}x{}_th{}.nc', 'iasi', 280, 340)
+
+    plot_dif(5, 5, 0,
+             'monthly_means/{}_{}_{}x{}_th{}.nc', -26, 13)
+
+    monthly_mean(1, 1, 0,
+                 'finished_data/2020_',
+                 'total_column', 'lon', 'lat', 'iasi')
+
+    plot_lv3_data(1, 1, 0,
+                  'monthly_means/iasi_{}_{}x{}_th{}.nc', 'iasi', 280, 340)
+
+    combined_plot(5, 5, 0, 'monthly_means/{}_{}_{}x{}_th{}.nc',
+                  'mean_tot', 280, 340)
--- main.py
+++ main.py
@@ -5,93 +5,65 @@ from os.path import join
 import os
 
 
-# file_path = "data/IASIA_MUSICA_030300_L2_AllTargetProducts_20200101000557_68496.nc"
+file_path = "data/IASIA_MUSICA_030300_L2_AllTargetProducts_20200101000557_68496.nc"
 
 # Open the dataset
-# dataset = nc.Dataset(file_path, mode='r')
+dataset = nc.Dataset(file_path, mode='r')
 
 # Print a list of all variable names
-# print("Variable names:")
-# for var_name in dataset.variables:
-#     print(var_name)
+print("Variable names:")
+for var_name in dataset.variables:
+    print(var_name)
 
 
-# def printvar(var_name):
-#     # Access the variable
-#     variable = dataset.variables[var_name]
-#
-#     # Print the variable's name
-#     print("Variable:", var_name)
-#
-#     # Print the variable's description (if available)
-#     if hasattr(variable, 'description'):
-#         print("Description:", variable.description)
-#
-#     # Print the variable's data
-#     print("Data:")
-#     if var_name in ['musica_ghg']:
-#         print(variable[:, 0][0])
-#     else:
-#         print(variable[0].filled(np.nan))  # Print all the values of the variable
-#
-#     if hasattr(variable, 'dimensions'):
-#         print("Dimensions:", variable.dimensions)
+def printvar(var_name):
+    # Access the variable
+    variable = dataset.variables[var_name]
+
+    # Print the variable's name
+    print("Variable:", var_name)
+
+    # Print the variable's description (if available)
+    if hasattr(variable, 'description'):
+        print("Description:", variable.description)
+
+    # Print the variable's data
+    print("Data:")
+    if var_name in ['musica_ghg']:
+        print(variable[:, 0][0])
+    else:
+        print(variable[:].shape)#.filled(np.nan))  # Print all the values of the variable#
+        #print(variable[0][0][:][:])
+        #print(variable[0])
 
 
-#printvar('musica_ghg')
-#printvar('musica_altitude_levels')
-#printvar('musica_pressure_levels')
+    if hasattr(variable, 'dimensions'):
+        print("Dimensions:", variable.dimensions)
+
 #printvar('musica_fit_quality_flag')
+printvar('musica_ghg_apriori')
+printvar('musica_ghg_avk_rank')
+printvar('musica_ghg_avk_val')
+printvar('musica_ghg_avk_lvec')
+printvar('musica_ghg_avk_rvec')
 
-#variable = dataset.variables['musica_altitude_levels'][:]
-#print(variable[0])
+rank = int(dataset.variables['musica_ghg_avk_rank'][0])
+eig_val = dataset.variables['musica_ghg_avk_val'][0][:rank]
+lvec = dataset.variables['musica_ghg_avk_lvec'][0][0][:rank][:].T
+rvec = dataset.variables['musica_ghg_avk_rvec'][0][0][:rank][:].T
 
-#variable = dataset.variables['musica_ghg'][:, 0]
-#print(variable[0])
+rxr_val = np.diag(eig_val)
 
-# count = 0
-# for i in range(variable.shape[0]):
-#     if float(0) not in variable[i, :]:
-#         count += 1
-#
-# print(variable.shape[0], count)
+avk = np.dot(np.dot(lvec, rxr_val), rvec.T)
 
-# for i in range(5, 10):
-#     print(variable[i, :])
+print(avk)
 
-#variable = dataset.variables['musica_fit_quality_flag']
+#print(np.diag(avk))
+
 
 # from convert_func import convert
 # convert()
 
 import gridding
-# gridding.grid_func.monthly_mean(5, 5, 0,
-#                                 'gosat_data/IUP-GHG-L2-N2O-GOSAT2-FOCAL-2020',
-#                                 'xn2o', 'longitude', 'latitude',
-#                                 'gosat')
-#
-# gridding.grid_func.monthly_mean(5, 5, 0,
-#                                 'finished_data/2020_',
-#                                 'total_column', 'lon', 'lat',
-#                                 'iasi')
-
-gridding.plot_lv3.plot_lv3_data(5, 5, 0,
-                                'monthly_means/gosat_{}_{}x{}_th{}.nc', 'gosat', 280, 340)
-
-gridding.plot_lv3.plot_lv3_data(5, 5, 0,
-                                'monthly_means/iasi_{}_{}x{}_th{}.nc', 'iasi', 280, 340)
-
-gridding.plot_dif(5, 5, 0,
-                  'monthly_means/{}_{}_{}x{}_th{}.nc', -26, 13)
-
-# gridding.grid_func.monthly_mean(1, 1, 0,
-#                                 'finished_data/2020_',
-#                                 'total_column', 'lon', 'lat',
-#                                 'iasi')
-
-gridding.plot_lv3.plot_lv3_data(1, 1, 0,
-                                'monthly_means/iasi_{}_{}x{}_th{}.nc', 'iasi', 280, 340)
-
-gridding.combined_plot(5, 5, 0, 'monthly_means/{}_{}_{}x{}_th{}.nc',
-                       'mean_tot', 280, 340)
+#gridding.replot_all()
 
