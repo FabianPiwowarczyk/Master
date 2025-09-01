602063091b93a7269f65938f7e6bd9e2069bb431
Fabian Piwowarczyk <fg.piwowarczyk@gmail.com>
2025-09-01 23:33:04 +0200

    update

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ file_path = "data/IASIA_MUSICA_030300_L2_AllTargetProducts_20200101000557_68496.
 dataset = nc.Dataset(file_path, mode='r')
 
 # Print a list of all variable names
-# print("Variable names:")
-# for var_name in dataset.variables:
-#    print(var_name)
+print("Variable names:")
+for var_name in dataset.variables:
+   print(var_name)
 
 
 def printvar(var_name):
@@ -30,7 +30,7 @@ def printvar(var_name):
     # Print the variable's data
     print("Data:")
     if var_name in ['musica_ghg']:
-        print(variable[:, 0][0])
+        print(len(variable[:, 0][0]))
     else:
         print(variable[:])
         #print(variable[0][0][:][:])
@@ -39,24 +39,25 @@ def printvar(var_name):
         print("Dimensions:", variable.dimensions)
 
 #printvar('musica_fit_quality_flag')
+printvar('musica_ghg')
 
 
-from convert_func import convert
-convert()
+#from convert_func import convert
+#convert()
 
-import gridding
-gridding.regrid_iasi()
+#import gridding
+#gridding.regrid_iasi()
 #gridding.replot_all()
 
 #from gridding.plot_lv3 import seasonal_plot
 #seasonal_plot()
 
-from concurrent.futures import ProcessPoolExecutor
-from functools import partial
+#from concurrent.futures import ProcessPoolExecutor
+#from functools import partial
 
 #inputs = [(1, 6), (7, 12)]  # Two different inputs
 
-import CAMS
+#import CAMS
 #CAMS.cams_monthly_means(x_res=5, y_res=5, months=[1, 12])
 
 # Use partial to fix a and b
@@ -68,8 +69,8 @@ import CAMS
 #from final_plots import find_one_specific_profile
 #find_one_specific_profile()
 
-import netCDF4 as nc
-import glob
+#import netCDF4 as nc
+#import glob
 
 # Find all matching files
 # files = glob.glob("monthly_means/cams_iasi_*_5x5_th0.nc")
