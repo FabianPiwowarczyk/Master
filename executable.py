74c097a1f0f6cdb147347bf714528065a3f56646
Fabian <fg.piwowarczyk@gmail.com>
2025-09-22 11:16:49 +0200

    new columns

--- executable.py
+++ executable.py
@@ -3,22 +3,16 @@
 
 def main():
     from CAMS.mean_cols import plot_mean_columns_multi
-    plot_mean_columns_multi(months=[6, 7, 8], coord=[(0, 30), (-65, -55)], idx=1)
 
-    # 1) Minimum over Canada: 60–65°N, 90–110°W, AMJ
-    plot_mean_columns_multi(months=[4, 5, 6], coord=[(-110, -90), (60, 65)], idx=2)
+    # 1) Rocky mountains: 35–40°N, 110–115°W, JAS
+    plot_mean_columns_multi(months=[7, 8, 9], coord=[(-115, -110), (35, 40)], idx=1)
 
-    # 2) Atlantic minumum pops up and vanishes: 40–45°N, 30–40°W, MAM
-    plot_mean_columns_multi(months=[3, 4, 5], coord=[(0, 30), (30, 40)], idx=3)
+    # 2) low over Siberia: 50–55°N, 75–80°E, OND
+    plot_mean_columns_multi(months=[10, 11, 12], coord=[(75, 80), (50, 55)], idx=2)
 
-    # 3) W. Pacific jet exit: 30–40°N, 130–160°E, SON
-    plot_mean_columns_multi(months=[9, 10, 11], coord=[(130, 160), (30, 40)], idx=4)
+    # 3) anomalies between Africa and south America: 20–25°S, 30–35°W, AMJ
+    plot_mean_columns_multi(months=[4, 5, 6], coord=[(-35, -30), (-25, 20)], idx=3)
 
-    # 4) Tibetan Plateau / W. China: 30–35°N, 80–95°E, JAS
-    plot_mean_columns_multi(months=[9, 10, 11], coord=[(80, 95), (30, 35)], idx=5)
-
-    # 7) SE Pacific stratocumulus: 15–25°S, 90–75°W, SON
-    plot_mean_columns_multi(months=[7, 8, 9], coord=[(-90, -75), (-25, -10)], idx=6)
 
 if __name__ == "__main__":
     main()
